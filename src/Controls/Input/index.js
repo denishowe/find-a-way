c20434e6528c49ab26ea2bc218335031127ad286
Denis Howe <denis.howe@gmail.com>
2019-01-06 12:04:18 +0000

    Upgrade react-dom

--- src/Controls/Input/index.js
+++ src/Controls/Input/index.js
@@ -1,37 +1,45 @@
 import React from "react";
-
+import styled from "styled-components";
 import { ucFirst } from "../../Utils";
 
 // An input with a type and a label
 
+const StyledInput = styled.input`
+    margin-left: 10px
+`;
+
+const StyledLabel = styled.label`
+    ${({ isNumber }) => isNumber && `margin: 20px`}
+`;
+
 class Input extends React.Component
 {
-	constructor(props)
-	{
-		super(props);
-		this.handleChange = this.handleChange.bind(this);
-	}
+    constructor(props)
+    {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-	handleChange(event)
-	{
-		const { name, value } = event.target;
-		this.props.setter(name, value);
+    handleChange(event)
+    {
+        const { name, value } = event.target;
+        this.props.setter(name, value);
 
-		return true;
-	}
+        return true;
+    }
 
-	render()
-	{
-		const { id, type, value } = this.props;
+    render()
+    {
+        const { id, type, value } = this.props;
 
-		return (
-			<label className={ `${type}-input` }>
-				{ ucFirst(id) }
-				<input type={ type } id={ id } name={ id } value={ value }
-					onChange={this.handleChange} />
-			</label>
-		);
-	}
+        return (
+            <StyledLabel isNumber={ type === "number" }>
+                { ucFirst(id) }
+                <StyledInput type={ type } id={ id } name={ id } value={ value }
+                    onChange={this.handleChange} />
+            </StyledLabel>
+        );
+    }
 }
 
 export default Input;
